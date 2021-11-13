1ed4c3d1393f9d009e2fbf43a50c57e6a0eb4f9a
nid989 <nidbhavsar989@gmail.com>
2021-11-13 09:41:29 +0530

    added changes

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY . .
 RUN pip install -r requirements.txt 
-CMD ["python3", "app.py"]
+CMD python3 app.py
--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
-
-
-@app.route('/login')
-def login():
     return render_template('login.html')
 
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
 
 @app.route('/success', methods=["POST", "GET"])
 def success():
@@ -22,5 +20,5 @@ def success():
         return render_template('home.html')
 
 
-if __name__ == 'main':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
