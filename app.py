15fe985d4082c82d8f80a0ad113dcbc37767c3a5
aakash0017 <akashbharat.bhatnagar@gmail.com>
2021-11-13 09:12:26 +0530

    added home page

--- app.py
+++ app.py
@@ -2,20 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
 @app.route('/login')
 def login():
-    return render_template('index.html')
+    return render_template('login.html')
 
-@app.route('/success', methods=["POST","GET"])
+
+@app.route('/success', methods=["POST", "GET"])
 def success():
     if request.method == "POST":
-        message = request.form
-        print(message["email"])
-        return f"{message} from login app."
+        # message = request.form
+        # print(message["email"])
+        # return f"{message} from login app."
+        return render_template('home.html')
+
 
 if __name__ == 'main':
-   app.run(debug = True)
+    app.run(debug=True)
