56e1201f9285e9064493b177614a1cdb40adba0d
aakash0017 <akashbharat.bhatnagar@gmail.com>
2022-01-29 14:08:15 +0530

    added mail facility

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from email.mime import text
-import random, string
+import emails
+import random
+import string
 from flask import Flask, render_template, request
 import smtplib
 from email.mime.text import MIMEText
@@ -7,36 +9,61 @@ from mail_generator import generate_mail
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('login.html')
+
 
 @app.route('/success', methods=["POST", "GET"])
 def success():
     if request.method == "POST":
         return render_template('home.html')
 
+
 @app.route('/minutes', methods=["POST", "GET"])
 def result():
     if request.method == "POST":
-        
-        sender = "nidbhavsar989@gmail.com"
+
+        message = emails.html(
+            html="<h1>This is an email</h1><strong>We love sending emails</strong>",
+            subject="Hey, look in here!",
+            mail_from="deepconteam@gmail.com",
+        )
+        # sender = "deepconteam@gmail.com"
         receivers = request.form.get("email")
-        receivers_name = request.form.get("name")
+        file = request.form.get("file")
+        # receivers_name = request.form.get("name")
 
-        subject, text = generate_mail(receivers_name)
-        message = MIMEText(text)
+        # subject, text = generate_mail(receivers_name)
+        # message = MIMEText(text)
 
-        message['Subject'] = subject
-        message['From'] = sender
-        message['To'] = receivers
+        # message['Subject'] = subject
+        # message['From'] = sender
+        # message['To'] = receivers
 
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login(sender, "15004959@Nb")
-        server.sendmail(sender, receivers, message.as_string())
+        # server = smtplib.SMTP(
+        #     "email-smtp.ap-south-1.amazonaws.com", 587)
+        # server.starttls()
+        # server.login(sender, "BFuujoxH6uJA3RYSZDIUDII3XTxJr4ReQweABpINju70")
+        # server.sendmail(sender, receivers, message.as_string())
 
+        r = message.send(
+            to=receivers,
+            smtp={
+                "host": "email-smtp.ap-south-1.amazonaws.com",
+                "port": 587,
+                "timeout": 5,
+                "user": "AKIA4QB2WTN5WRHMWS4U",
+                "password": "BFuujoxH6uJA3RYSZDIUDII3XTxJr4ReQweABpINju70",
+                "tls": True,
+            }
+        )
+
+        print(r.status_code)
+        print(file)
         return render_template('result.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host='127.0.0.1')
