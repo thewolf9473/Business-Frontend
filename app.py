a191fd5423b51943d5ade09ec82dd8eafb1d892e
aakash0017 <akashbharat.bhatnagar@gmail.com>
2022-04-09 11:42:02 +0530

    minor changes

--- app.py
+++ app.py
@@ -3,26 +3,24 @@ import random
 import string
 from flask import Flask, render_template, request, redirect
 import requests
-from mail_generator import send_email
+from mail_generator import generate_mail
 from utilities import upload_to_aws
 import os
-import asyncio
-import httpx
+import asyncio, httpx
 from dotenv import load_dotenv
-from database_handler import insert_values
 load_dotenv()
 
 
 app = Flask(__name__)
 
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_USER_PASSWORD = os.getenv("EMAIL_USER_PASSWORD")
+
+
 app.config["MP3_UPLOADS"] = "static/images/uploads"
-
-
 def generate_process_code():
-    x = ''.join(random.choice(string.ascii_uppercase +
-                string.ascii_lowercase + string.digits) for _ in range(16))
+    x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
     return x
-
 
 @app.route('/')
 def index():
@@ -38,41 +36,55 @@ def success():
 @app.route('/minutes', methods=["POST", "GET"])
 async def result():
     if request.method == "POST":
-
+        
         sender = "deepconteam@gmail.com"
         receivers = request.form.get("email")
         file = request.files["audio"]
         file.save(os.path.join(app.config["MP3_UPLOADS"], file.filename))
         file_path = "static/images/uploads/{}".format(file.filename)
         print("-------------file path -------------- ", file_path)
+        
+            
         receivers_name = request.form.get("name")
         process_code = generate_process_code()
-        res = upload_to_aws(file_path, file_name=process_code)
 
+        subject, text = generate_mail(receivers_name, process_code)
+        res = upload_to_aws(file_path, file_name=process_code )
+        print(res)
+       
+
+        message = emails.html(
+            text=text,
+            subject=subject,
+            mail_from=sender,
+        )
+        
         try:
-
+        
+            # req = requests.post('http://server-service.default.svc.cluster.local:8000/getcode',
+            #                 params={'process_code': process_code})
+            
             async with httpx.AsyncClient() as client:
-                params_dict = {'process_code': process_code,
-                               'receiver_email': receivers,
-                               'receiver_name': receivers_name
-                               }
                 res = await asyncio.gather(
-                    client.post('http://localhost:8000/getcode',
-                                params=params_dict)
+                    client.post('http://localhost:8000/getcode', params={'process_code': process_code})
+                    # client.post('http://server-service.default.svc.cluster.local:8000/getcode', params={'process_code': process_code})
                 )
-
+                # req = requests.post()
         except:
             print("microservice request not processed")
+     
 
-        insert_values(process_code=process_code,
-                      receiver_name=receivers_name,
-                      receiver_email=receivers,
-                      )
-
-        email_res = send_email(process_code=process_code,
-                               receiver_email=receivers,
-                               receivers_name=receivers_name,
-                               sender=sender)
+        r = message.send(
+            to=receivers,
+            smtp={
+                "host": "email-smtp.ap-south-1.amazonaws.com",
+                "port": 587,
+                "timeout": 5,
+                "user": EMAIL_USER,
+                "password": EMAIL_USER_PASSWORD,
+                "tls": True,
+            }
+        )
 
         return render_template('result.html')
 
--- mail_generator.py
+++ mail_generator.py
@@ -1,8 +1,4 @@
-import os
 import random, string
-import emails
-import os
-
 
 
 def generate_process_code():
@@ -11,39 +7,14 @@ def generate_process_code():
 
 def generate_mail(user_name="user", process_code=""):
 
+    # process_code = generate_process_code()
     SUBJECT = "[ALERT] DeepCon upload process code."
     TEXT = f"Hello {user_name}, We're glad that you've chosen DeepCon. Here is your process code: {process_code}"
 
     return SUBJECT, TEXT
 
-def send_email(receivers_name, process_code, receiver_email, sender):
-    
-    EMAIL_USER = os.getenv("EMAIL_USER")
-    EMAIL_USER_PASSWORD = os.getenv("EMAIL_USER_PASSWORD")      
-    print(receivers_name)
-    print(receiver_email)
-    print(sender)
-    subject, text = generate_mail(receivers_name, process_code)
-    print(text)
-    message = emails.html(
-            text=text,
-            subject=subject,
-            mail_from=sender,
-        )
-    
-    r = message.send(
-            to=receiver_email,
-            smtp={
-                "host": "email-smtp.ap-south-1.amazonaws.com",
-                "port": 587,
-                "timeout": 5,
-                "user": EMAIL_USER,
-                "password": EMAIL_USER_PASSWORD,
-                "tls": True,
-            }
-        )
-    
-    return r
+if __name__ == "__main__":
+    print(generate_mail("nidbhavsar989@gmail.com", "nidbhavsar4959@gmail.com"))
 
     
 
